5a12fed6613114e1d77141513c02cbf3f63fd366
Ubuntu <peppe.manzi1@live.it>
2017-05-15 12:37:08 +0200

    added comments to map_iterator.h

--- src/map_iterator.h
+++ src/map_iterator.h
@@ -3,10 +3,14 @@
 
 #include "map.h"
 
+//inizialize an iterator for the map
 void* map_it_init(map_t map);
 
+//return 1 if there are more elements on which we can iterate
 int map_it_hasnext(map_t map, void* state);
 
+//return the key of the next element.
+//Requires a pointer to the iterator state
 void* map_it_next(map_t map, void** state);
 
 #endif
